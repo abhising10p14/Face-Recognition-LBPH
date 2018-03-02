052af704a6f632e5c63c8a767e9799e07b71bed2
abhising10p14 <abhisingh10p14@gmail.com>
2018-03-03 00:38:31 +0530

    Added the exception for non-frontal faces

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import train as tr
 import mapping as mp
 import cv2
 import numpy as np
+import sys 
 #function to draw rectangle on image 
 #according to given (x, y) coordinates and 
 #given width and heigh
@@ -34,15 +35,18 @@ face_recognizer.train(dc.faces, np.array(dc.labels))
 def predict(test_img):
 	#make a copy of the image as we don't want to change original image
 	img = test_img
+	#print height, width, channels
 	#detect face from the image
 	face, rect = detect.detect_face(img)
-	 
-	print "Here"
+	if face is None:
+		print "couldn't find a face in the test image"
+		print "Please provide a clear frontal face image "
+		sys.exit()
 	#predict the image using our face recognizer 
 	label, confidence = face_recognizer.predict(face)
 	#get name of respective label returned by face recognizer
-	print "There"
 	label_text = mp.subjects[label]
+	print "The detected person is: " + label_text
 	 
 	#draw a rectangle around face detected
 	draw_rectangle(img, rect)
@@ -57,18 +61,19 @@ print("Predicting images...")
  
 #load test images
 test_img1 = cv2.imread("test-data/test1.jpg")
-test_img2 = cv2.imread("test-data/test12.jpg")
-test_img3 = cv2.imread("test-data/test3.jpeg")
+test_img2 = cv2.imread("test-data/test3.jpeg")
+test_img3 = cv2.imread("test-data/test31.jpg")
  
 #perform a prediction
-#predicted_img1 = predict(test_img1)
+predicted_img1 = predict(test_img1)
+predicted_img2 = predict(test_img2)
 predicted_img3 = predict(test_img3)
-#predicted_img2 = predict(test_img2)
+
 print("Prediction complete")
  
 #display both images
-#cv2.imshow("figure",predicted_img1)
-#cv2.imshow("figure",predicted_img2)
+cv2.imshow("figure",predicted_img1)
+cv2.imshow("figure",predicted_img2)
 cv2.imshow("figure",  cv2.resize(predicted_img3, (400, 500)))
 
 cv2.waitKey(0)
